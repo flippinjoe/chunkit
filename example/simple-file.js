096fc914f6fc5db97cd97bec8b2e7f1adbf06115
Joe Ridenour <joerdnr19@gmail.com>
2016-08-15 11:46:28 -0500

    Updates
    - Added examples of parsing
    - Implemented callback on streamer

---
+++ example/simple-file.js
@@ -0,0 +1,11 @@
+
+var chunkit = require('../index');
+
+var parser = new chunkit.Parser();
+parser.chunkFile(__dirname + '/resources/big-image.jpg')
+  .on('chunk', function (idx) {
+    console.log("Chunk: ", idx);
+  })
+  .on('end', function () {
+    console.log("done");
+  })
